8277d198a6193c7a92a72aeecfb9038ae05d0b89
Axelux <126026128+AxelusSS@users.noreply.github.com>
2024-04-04 20:55:00 +0200

    Add files via upload

---
+++ public/index.php
@@ -0,0 +1,14 @@
+<?php
+
+use App\Kernel;
+try{    
+    require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
+    return function (array $context) {
+    return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
+    };
+}
+finally{
+    echo "Feur man";
+}
+
+
